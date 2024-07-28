fc82ed48fbb4b98d892a4b89ffe7bb040699c90e
Silvia Chang <sunny1218.chang@gmail.com>
2024-07-28 14:36:09 +1000

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Sunny on 2024/7/28.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
